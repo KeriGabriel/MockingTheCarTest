574254b3b5748fe8b5eb0df670a884b105f38098
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-06-09 11:47:22 -0700

    add second test method

--- MocTest/UnitTest1.cs
+++ MocTest/UnitTest1.cs
@@ -4,45 +4,25 @@ using Moq;
 
 namespace MocTest
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestEngine()
-        {
-            var vehicle = new Mock<Vehicle>();
-            var engine = new Mock<Engine>();
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestEngine()
+		{
 
-        }
-    }
+		}
+
+		[TestMethod]
+		public void AndreaTestEngine()
+		{
+
+			var vehicle = new Mock<Vehicle>();
+			var engine = new Mock<Engine>();
+
+			//engine.Setup(e => e.(1, "type", "amout")).Returns(false);
+			//vehicle.Object.Validate("hi", "fuel", "amount");
+		}
+
+	}
 }
-
-
-
-/*
- * Andrea testing notes
-
-public void TestEngine()
-{
-	// need to see if the Engine starts
-	// Test constructor function for Vehicle(Engine)
-	// Calls bool::Start()
-	// Does Start(return true)
-}
-
-
-[TestMethod]
-public void TestMethod1()
-{
-
-	var person = new Mock<Person>();
-	person.Setup(p => p.Validate(
-		 It.IsAny<string>(),
-		 21,
-		 It.IsAny<string>())).Returns(false);
-	Assert.IsFalse(
-		 person.Object.Validate("Jordan", 21, "Russia"));
-
-}
-
-*/
